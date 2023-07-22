8e08a378b34557338e4a09dc0478e23ea0e6c14a
DeadTree <lhwltwc@163.com>
2023-07-22 10:00:34 +0800

    v0.1 添加测试类，配置文件

---
+++ kumu-blog/src/main/java/com/kumu/KumuBlogApplication.java
@@ -0,0 +1,11 @@
+package com.kumu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumuBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KumuBlogApplication.class,args);
+    }
+}
