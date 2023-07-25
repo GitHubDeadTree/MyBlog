ab9ecb35e3a49a9247e8a0c231bd60115779cec2
DeadTree <lhwltwc@163.com>
2023-07-25 11:12:22 +0800

    v1.1.2 完成友链接口

--- kumu-framework/src/main/java/com/kumu/service/impl/LinkServiceImpl.java
+++ kumu-framework/src/main/java/com/kumu/service/impl/LinkServiceImpl.java
@@ -1,11 +1,17 @@
 package com.kumu.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kumu.constants.SystemConstants;
 import com.kumu.domain.ResponseResult;
 import com.kumu.domain.entity.Link;
+import com.kumu.domain.vo.LinkVo;
 import com.kumu.mapper.LinkMapper;
 import com.kumu.service.LinkService;
+import com.kumu.utils.BeanCopyUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 友链(Link)表服务实现类
@@ -18,9 +24,12 @@ public class LinkServiceImpl extends ServiceImpl<LinkMapper, Link> implements Li
 
     @Override
     public ResponseResult getFriendLink() {
-
-
-        return ResponseResult.okResult();
+        //查询所有审核通过的link
+        LambdaQueryWrapper<Link> queryWrapper= new LambdaQueryWrapper<>();
+        queryWrapper.eq(Link::getStatus, SystemConstants.LINK_STATUS_NORMAL);
+        List<Link> linkList = list(queryWrapper);
+        List<LinkVo> Vos = BeanCopyUtils.copyBeanList(linkList, LinkVo.class);
+        return ResponseResult.okResult(Vos);
     }
 }
 
