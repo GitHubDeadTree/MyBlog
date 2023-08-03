9faa539116e53bc79b4c89501b103147e0a972bf
DeadTree <lhwltwc@163.com>
2023-08-03 11:26:48 +0800

    v1.2.3 验证用户名

--- kumu-framework/src/main/java/com/kumu/service/impl/BlogLoginServiceImpl.java
+++ kumu-framework/src/main/java/com/kumu/service/impl/BlogLoginServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class BlogLoginServiceImpl implements BlogLoginService {
 
@@ -19,6 +21,10 @@ public class BlogLoginServiceImpl implements BlogLoginService {
         //封装传入的用户名和密码，以便后续使用
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword());
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+        //判断是否认证通过
+        if (Objects.isNull(authenticate)) {
+            throw new RuntimeException("用户名或密码错误");
+        }
         return null;
     }
 }
--- kumu-framework/src/main/java/com/kumu/service/impl/UserDetailsServiceImpl.java
+++ kumu-framework/src/main/java/com/kumu/service/impl/UserDetailsServiceImpl.java
@@ -1,4 +1,35 @@
 package com.kumu.service.impl;
 
-public class UserDetailsServiceImpl {
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.kumu.domain.entity.LoginUser;
+import com.kumu.domain.entity.User;
+import com.kumu.mapper.UserMapper;
+import com.mysql.cj.log.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //根据用户名查询用户信息
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUserName,username);
+        User user = userMapper.selectOne(queryWrapper);
+        //是否查出用户？ 如果没查到查出异常
+        if (Objects.isNull(user)){
+            throw new RuntimeException("用户不存在");
+        }
+        //返回用户信息
+        //TODO 查询用户权限信息封装
+        return new LoginUser(user);
+    }
 }
