82a62d601db74ce11475bc0bfd41c04d944734b9
DeadTree <lhwltwc@163.com>
2023-08-05 11:59:01 +0800

    v1.4.2 回滚查询友链所需权限

--- kumu-blog/src/main/java/com/kumu/config/SecurityConfig.java
+++ kumu-blog/src/main/java/com/kumu/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/login").anonymous()
+                .antMatchers("/link/getAllLink").authenticated()
                 //注销接口需要认证才能访问
                 .antMatchers("/logout").authenticated()
                 // 除上面外的所有请求全部不需要认证即可访问
